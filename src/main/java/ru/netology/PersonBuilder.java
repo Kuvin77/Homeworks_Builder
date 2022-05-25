daa3d0382dc7fd737d3b8b56d81c930b92d7315a
Kuvin77 <v.kustyshev@gmail.com>
2022-05-25 17:29:58 +0300

    1 commit

---
+++ src/main/java/ru/netology/PersonBuilder.java
@@ -0,0 +1,55 @@
+package ru.netology;
+
+public class PersonBuilder {
+    protected String name;
+    protected String surname;
+    protected int age = 5;
+    protected String address;
+
+    public PersonBuilder setName(String name) throws IllegalArgumentException {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Не заполнено имя");
+        } else {
+            this.name = name;
+        }
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) throws IllegalArgumentException {
+        if (surname == null || surname.isEmpty()) {
+            throw new IllegalArgumentException("Не заполнена фамилия");
+        } else {
+            this.surname = surname;
+        }
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) throws IllegalArgumentException {
+        if (age < 0) {
+            throw new IllegalArgumentException("Возраст не может быть меньше 0");
+        } else {
+            this.age = age;
+        }
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() throws IllegalStateException {
+        Person person;
+        if (name == null || surname == null) {
+            throw new IllegalStateException("Имя или фамилия не указаны");
+        }
+        if (age < 0) {
+            person = new Person(name, surname);
+        } else {
+            person = new Person(name, surname, age);
+        }
+        person.setAddress(address);
+        return person;
+    }
+}
+
